c27a0036efa33aa71941f6bb428d6db6fa6a3e11
Sev Furneaux <sevfurneaux@gmail.com>
2014-10-10 17:19:32 +0100

    Added field 'image_name' to Entry model

--- app/admin/entry.rb
+++ app/admin/entry.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Entry do
-  permit_params :day, :month, :year, :body
+  permit_params :day, :month, :year, :body, :image_name
 
   index do
     column :id
@@ -7,5 +7,16 @@ ActiveAdmin.register Entry do
     column :month
     column :year
     actions
+  end
+
+  form do |f|
+    f.inputs "Entry" do
+      f.input :day
+      f.input :month
+      f.input :year
+      f.input :body
+      f.input :image_name
+    end
+    f.actions
   end
 end
