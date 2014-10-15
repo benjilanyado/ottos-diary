a7b704394b015e0c84e011132519388316e7cf68
Sev Furneaux <sevfurneaux@gmail.com>
2014-10-15 23:41:04 +0100

    Initial work on months integrated with sidebar years

--- app/models/date_composer.rb
+++ app/models/date_composer.rb
@@ -2,6 +2,7 @@ class DateComposer
   attr_accessor :entries
 
   YEARS = ['1915', '1916', '1917', '1918', '1919', '1920']
+  MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
   def initialize(entries)
     @entries = entries
@@ -11,7 +12,17 @@ class DateComposer
     objects = []
 
     YEARS.each do |year|
-      objects.push(OpenStruct.new(year: year, entries: entries.where(year: year).order('created_at ASC')))
+      entries_by_year = entries.where(year: year).order('created_at ASC')
+      collated_months = { 'January' => [], 'February' => [], 'March' => [], 'April' => [], 'May' => [], 'June' => [], 'July' => [], 'August' => [], 'September' => [], 'October' => [], 'November' => [], 'December' => []}
+      entries_by_year.each do |entry|
+        MONTHS.each do |month|
+          if entry.month == month
+            collated_months[month].push(entry)
+          end
+        end
+      end
+
+      objects.push(OpenStruct.new(year: year, entries: collated_months))
     end
 
     objects
